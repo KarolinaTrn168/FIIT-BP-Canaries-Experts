49b929e7eed21a782a8d7a7680635e7c7a7e8def
KarolinaTrn168 <32123046+KarolinaTrn168@users.noreply.github.com>
2020-04-01 17:03:40 +0200

    Update connection.py

--- connection.py
+++ connection.py
@@ -4,7 +4,7 @@ import redis
 #toto potom do nejakeho konfig filu
 def connection_db():
     try:
-        r = redis.StrictRedis(host='cubemail.ga', port=6379, password='7hA~KR&)tLe@Cbbh|=#dS9xV')
+        r = redis.StrictRedis(host='cubemail.ga', port=6379, password='.....')
         #print(r)
         r.ping()
         print('Connected')
